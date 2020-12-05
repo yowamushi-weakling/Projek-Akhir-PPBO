a8d649f6bdf44e97d72649307d463dde6b1b1dd9
yowamushi-weakling <73294938+yowamushi-weakling@users.noreply.github.com>
2020-12-05 11:33:18 +0700

    Menu
    Making Menu

---
+++ Projek Akhir PPBO/Assets/Script/UIHome.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIHome : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OptionGame()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit!");
+        Application.Quit();
+    }
+}
