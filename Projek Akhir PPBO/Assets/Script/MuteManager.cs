f24ccf5c9c8a88f44b336a693743ffc6d04ab9ef
yowamushi-weakling <73294938+yowamushi-weakling@users.noreply.github.com>
2020-12-09 15:57:57 +0700

    Main Menu Complete

---
+++ Projek Akhir PPBO/Assets/Script/MuteManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MuteManager : MonoBehaviour
+{
+    private bool isMuted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMuted = false;
+    }
+
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+    }
+}
---
+++ Projek Akhir PPBO/Assets/Script/btnSFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnSFX : MonoBehaviour
+{
+    public AudioSource mySFx;
+    public AudioClip hoverSFx;
+    public AudioClip clickSFx;
+
+    public void HoverSound()
+    {
+        mySFx.PlayOneShot(hoverSFx);
+    }
+    public void ClickSound()
+    {
+        mySFx.PlayOneShot(clickSFx);
+    }
+}
