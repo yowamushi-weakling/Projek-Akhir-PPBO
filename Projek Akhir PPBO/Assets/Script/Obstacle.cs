e630d7abb280c2582b98cfeac3ead11b1c08c3c0
yowamushi-weakling <73294938+yowamushi-weakling@users.noreply.github.com>
2020-12-12 16:31:54 +0700

    Gamplay Finished (without UI)

---
+++ Projek Akhir PPBO/Assets/Script/Obstacle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+    public GameObject effect;
+    private Animator camAnim;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {            
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            // Damaging Player Health
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Projek Akhir PPBO/Assets/Script/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+
+    public GameObject effect;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            Instantiate(effect, transform.position, Quaternion.identity);
+            // Ketika menekan tombol panah atas, targetPos berpindah tempat sebesar x = 0 , y = 0 + Yincrement
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            Instantiate(effect, transform.position, Quaternion.identity);
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+}
